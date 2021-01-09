b82a1c767517fc17f9b77c79b1ca05ad37882f3d
sergiouph <sergio.pedraza@clipdashboard.com>
2021-01-08 18:56:33 -0600

    Add npm+webpack+ts+react stuff

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.js"
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: "ts-loader" }
+      ]
+    }
+  };
